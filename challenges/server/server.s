7380203c7cfc8bca96a50adabbf66f5230aa5504
Umar Rustamov <rustamovumar0@gmail.com>
2025-09-15 20:39:38 +0500

    server task listen command

---
+++ challenges/server/server.s
@@ -0,0 +1,34 @@
+.intel_syntax noprefix
+.globl _start
+
+.section .text
+
+_start:
+    mov rdi, 2
+    mov rsi, 1
+    xor rdx, rdx
+    mov rax, 41
+    syscall
+    mov r8, rax
+
+    push 0x00000000
+    push 0x00000000
+    push 0x00000000
+    push 0x50000002
+
+    mov rdi, r8
+    mov rsi, rsp
+    mov rdx, 16
+    mov rax, 49
+    syscall
+
+    mov rdi, r8
+    xor rsi, rsi
+    mov rax, 50
+    syscall
+
+    mov rdi, 0
+    mov rax, 60
+    syscall
+
+.section .data
