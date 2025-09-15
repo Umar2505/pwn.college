ae313547e3a6791121ec6dda322f1d8bb9b4343b
Umar Rustamov <rustamovumar0@gmail.com>
2025-09-15 23:24:39 +0500

    server task added read and write

--- challenges/server/server.s
+++ challenges/server/server.s
@@ -11,6 +11,7 @@ _start:
     syscall
     mov r8, rax
 
+    mov rbp, rsp
     push 0x00000000
     push 0x00000000
     push 0x00000000
@@ -31,6 +32,38 @@ _start:
     mov rsi, 0
     mov rdx, 0
     mov rax, 43
+    syscall
+
+    mov r8, rax
+
+    mov rax, 0x00000A0D0A0D302E
+    push rax
+    mov rax, 0x312F505454482067
+    push rax
+    mov rax, 0x616C662F20544547
+    push rax
+    
+    mov rdi, r8
+    mov rsi, rsp 
+    mov rdx, 256
+    mov rax, 0
+    syscall
+
+    mov rax, 0x00000000000A0D0A
+    push rax
+    mov rax, 0x0D4B4F2030303220
+    push rax
+    mov rax, 0x302E312F50545448
+    push rax
+
+    mov rdi, r8
+    mov rsi, rsp
+    mov rdx, 19
+    mov rax, 1
+    syscall
+
+    mov rdi, r8
+    mov rax, 3
     syscall
 
     mov rdi, 0
