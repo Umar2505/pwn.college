1e6df75f0d8fd858d0ae18edd84e10c92d452a8a
Umarbek Rustamov <77962976+Umar2505@users.noreply.github.com>
2025-09-25 21:27:45 +0300

    Server on Assembly that works concurrently and accepts both POST and GET methods

--- challenges/server/server.s
+++ challenges/server/server.s
@@ -1,15 +1,28 @@
 .intel_syntax noprefix
 .globl _start
 
+.section .bss
+buffer:
+    .skip 500     # buffer to read stdin
+file_content:
+    .skip 256
+
+.section .data
+mystring:
+    .ascii "HTTP/1.0 200 OK\r\n\r\n"    # string, no null terminator
+comp_str:
+    .ascii "\r\n\r\n"
+
 .section .text
 
 _start:
+    # socket
     mov rdi, 2
     mov rsi, 1
     xor rdx, rdx
     mov rax, 41
     syscall
-    mov r8, rax
+    mov r12, rax
 
     mov rbp, rsp
     push 0x00000000
@@ -17,57 +30,183 @@ _start:
     push 0x00000000
     push 0x50000002
 
-    mov rdi, r8
+    # bind
+    mov rdi, r12
     mov rsi, rsp
     mov rdx, 16
     mov rax, 49
     syscall
 
-    mov rdi, r8
+    # listen
+    mov rdi, r12
     xor rsi, rsi
     mov rax, 50
     syscall
 
-    mov rdi, r8
+parent:
+    # accept
+    mov rdi, r12
     mov rsi, 0
     mov rdx, 0
     mov rax, 43
     syscall
+    mov rbx, rax
 
-    mov r8, rax
-
-    mov rax, 0x00000A0D0A0D302E
-    push rax
-    mov rax, 0x312F505454482067
-    push rax
-    mov rax, 0x616C662F20544547
-    push rax
-    
-    mov rdi, r8
-    mov rsi, rsp 
-    mov rdx, 256
-    mov rax, 0
+    # fork
+    mov rax, 57
     syscall
 
-    mov rax, 0x00000000000A0D0A
-    push rax
-    mov rax, 0x0D4B4F2030303220
-    push rax
-    mov rax, 0x302E312F50545448
-    push rax
+    cmp rax, 0
+    jl error
+    je server
 
+    mov rdi, rbx
+    mov rax, 3
+    syscall
+
+    jmp parent
+
+server:
+    mov rdi, r12
+    mov rax, 3
+    syscall
+    
+    # read into buffer
+    mov rax, 0          # syscall: read
+    mov rdi, rbx         # fd = stdin
+    lea rsi, [buffer]   # pointer to buffer
+    mov rdx, 500       # max bytes to read
+    syscall
+
+    mov rcx, rax        # rcx = number of bytes read
+    mov r13, rax
+    # --- skip first part until first space ---
+    lea rdi, [buffer]   # rdi = pointer to buffer start
+    xor r9, r9          # counter
+skip_first_space:
+    cmp rcx, 0
+    je find_comp_str    # if buffer empty, done
+    mov al, [rdi]
+    cmp al, ' '         # check for space
+    je found_first_space
+    inc r9
+    inc rdi
+    dec rcx
+    jmp skip_first_space
+
+found_first_space:
+    inc r9
+    inc rdi             # move past the space
+    dec rcx             # adjust remaining bytes
+    mov r8, rdi         # r8 = start of path
+
+    # --- find second space to null-terminate ---
+find_second_space:
+    cmp rcx, 0
+    je find_comp_str             # end if no more bytes
+    mov al, [rdi]
+    cmp al, ' '
+    je terminate_path
+    inc rdi
+    dec rcx
+    inc r9
+    jmp find_second_space
+terminate_path:
+    mov r15, rdi
+
+# --- get the content of a file ---
+find_comp_str:
+    mov rax, [rdi]
+    cmp eax, dword ptr [comp_str]         # check for space
+    je found_comp_str
+    inc r9
+    inc rdi
+    dec rcx
+    jmp find_comp_str
+
+found_comp_str:
+    add r9, 4
+    sub r13, r9
+    add rdi, 4             # move past the space
+    sub rcx, 4             # adjust remaining bytes
+    mov r14, rdi         # r8 = start of path
+    xor r9, r9           # counter
+
+
+done:
+    mov byte ptr [r15], 0x00
+    mov al, byte ptr [buffer]
+    cmp al, 'P'
+    je post
+    jmp get
+post:
+# open
+    mov rdx, 0777
     mov rdi, r8
-    mov rsi, rsp
-    mov rdx, 19
+    mov rsi, 0x41
+    mov rax, 2
+    syscall
+    mov r8, rax
+# write opened file
+    mov rdi, rax
+    mov rsi, r14
+    mov rdx, r13
     mov rax, 1
     syscall
-
-    mov rdi, r8
+    mov r10, rax
+# close file
     mov rax, 3
+    mov rdi, r8
+    syscall
+
+# write
+    mov rdi, rbx
+    lea rsi, [mystring]
+    mov rdx, 19
+    mov rax, 1
     syscall
 
     mov rdi, 0
     mov rax, 60
     syscall
 
-.section .data
+get:
+# open
+    mov rdi, r8
+    mov rsi, 0
+    mov rax, 2
+    syscall
+    mov r8, rax
+# read opened file
+    mov rdi, rax
+    lea rsi, [file_content]
+    mov rdx, 256
+    mov rax, 0
+    syscall
+    mov r10, rax
+# close file
+    mov rax, 3
+    mov rdi, r8
+    syscall
+
+# write
+    mov rdi, rbx
+    lea rsi, [mystring]
+    mov rdx, 19
+    mov rax, 1
+    syscall
+
+    mov rdi, rbx
+    lea rsi, [file_content]
+    mov rdx, r10
+    mov rax, 1
+    syscall
+
+    mov rdi, 0
+    mov rax, 60
+    syscall
+error:
+    mov rdi, 1
+    mov rax, 60
+    syscall
+
